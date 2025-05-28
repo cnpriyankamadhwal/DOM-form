f587d6d42ed5d65f6d1ff3bf93249649619a30b7
Priyanka Madhwal <1nonlyavniweb@gmail.com>
2025-05-28 19:36:53 +0530

    half work

---
+++ Script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+const form=document.getElementById('studentForm');
+const addBtn=document.getElementById('addBtn');
+const removeBtn=document.getElementById('removeBtn');
+const resetBtn=document.getElementById('resetBtn');
+const registeredStudent=document.getElementById('registeredStudent');
+
+addBtn.addEventListener('click', function (){
+        const fullName=document.getElementById('fullName');
+        const age=document.getElementById('age');
+        const email=document.getElementById('email');
+
+        if(fullName && age && email){
+            const newRow=document.createElement('tr');
+            newRow.
+        }
+});
+
+});
