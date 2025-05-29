f2473f5977ad447c6ad0097b1aa368960d3647c0
Priyanka Madhwal <1nonlyavniweb@gmail.com>
2025-05-29 19:21:26 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,19 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
-const form=document.getElementById('studentForm');
-const addBtn=document.getElementById('addBtn');
-const removeBtn=document.getElementById('removeBtn');
-const resetBtn=document.getElementById('resetBtn');
-const registeredStudent=document.getElementById('registeredStudent');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('studentForm');
+    const addBtn = document.getElementById('addBtn');
+    const removeBtn = document.getElementById('removeBtn');
+    const resetBtn = document.getElementById('resetBtn');
+    const tableBody = document.getElementById('tableBody');
 
-addBtn.addEventListener('click', function (){
-        const fullName=document.getElementById('fullName');
-        const age=document.getElementById('age');
-        const email=document.getElementById('email');
+    addBtn.addEventListener('click', function () {
+        const fullName = document.getElementById('fullName').value;
+        const age = document.getElementById('age').value;
+        const email = document.getElementById('email').value;
 
-        if(fullName && age && email){
-            const newRow=document.createElement('tr');
-            newRow.
+        if (fullName && age && email) {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                        <td>${fullName}</td>
+                        <td>${email}</td>
+                        <td>${age}</td>
+                    `;
+            tableBody.appendChild(newRow);
+            form.reset();
         }
-});
+        else {
+            alert('Please fill up all fields correctly.');
+        }
+    });
 
+    // removeBtn...
+    removeBtn.addEventListener('click', function () {
+        if (tableBody.lastChild) {
+            tableBody.removeChild(tableBody.lastChild);
+        }
+    });
+
+    // resetBtn...
+    resetBtn.addEventListener('click', function () {
+        form.reset();
+    });
 });
